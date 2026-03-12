abdd3f81a6da9e99f1298a972aa726fe6760e29a
Niviel <202130101@daelim.ac.kr>
2026-03-12 16:28:51 +0900

    week02 v0.2

--- week02.py
+++ week02.py
@@ -1,9 +1,6 @@
 
 scores = [100, 97, 88, 91]
-hap, count = 0,0
-for score in scores:
-    hap = hap + score
-    count = count + 1
 
-average = hap / count
+
+average = sum(scores) / len(scores)
 print(average)
