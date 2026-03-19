db3e19c28d9d82b577a95562df867adbed66eaa0
Niviel <202130101@daelim.ac.kr>
2026-03-19 14:42:49 +0900

    week03 v0.4

--- week02.py
+++ week02.py
@@ -1,5 +1,6 @@
-import statistics
+import statistics #( 파이썬 내장 모듈 느림..)
 scores = [100, 97, 88, 91]
 
 average = statistics.mean(scores)
 print(average)
+
---
+++ week03.py
@@ -0,0 +1,5 @@
+import numpy #(써드파티 라이브러리, c기반으로 대규모 데이터 처리 매우빠름 )
+
+scores = [100,97,88,91]
+average = numpy.mean(scores)
+print(average)
